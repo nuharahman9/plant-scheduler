f82d46ded4e484f71a4ead4dbb6ce64e95b406bb
nuharahman9 <nrahm009@ucr.edu>
2021-07-07 15:51:11 -0700

    Reg and login

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -1,35 +1,73 @@
 import React, { useState, useEffect } from 'react'; 
 import "firebase/auth"; 
 import fire from './config'; 
+import "./Login.css"; 
 import Home from '../Homepage/homepage'; 
 
 const Login = () => { 
+  const [currentUser, setCurrentUser] = useState(''); 
   const [enteredEmail, setEmail] = useState('');
   const [enteredPassword, setPassword] = useState(''); 
-  const [currentUser, setCurrentUser] = useState(''); 
- // const [hasAccount, setHasAccount] = useState(false); 
-  
+  const [hasAccount, setHasAccount] = useState(true); 
+  const [emailError, setEmailError] = useState(''); 
+  const [passwordError, setPasswordError] = useState(''); 
+
   const emailHandler = (event) => { 
     setEmail(event.target.value); 
   };
   const passwordHandler = (event) => { 
     setPassword(event.target.value); 
   };
+  const clearErrors = () => { 
+    setEmailError(''); 
+    setPasswordError(''); 
+  };
+ // const clearInput = () => { 
+   // setPassword(''); 
+ // }; 
 
   const LoginHandler = (event) => {
     event.preventDefault(); 
+    clearErrors(); 
     fire.auth().signInWithEmailAndPassword(enteredEmail, enteredPassword)
     .catch(error => { 
-        setPassword(''); 
-        //change later for individual cases 
-    });  
-    
-  //  console.log(LoginData); 
+        switch(error.code) { 
+          case "auth/invalid-email":
+          case "auth/user-not-found":
+            setEmailError(error.message); 
+            break; 
+          case "auth/wrong-password":
+            setPasswordError(error.message);
+            break;
+          default: 
+            
 
+        }
+    });  
   };
 
+  const RegistrationHandler = (event) => {
+    event.preventDefault(); 
+    clearErrors(); 
+    fire.auth().createUserWithEmailAndPassword(enteredEmail, enteredPassword)
+    .catch(error => { 
+        switch(error.code) { 
+          case "auth/invalid-email":
+          case "auth/email-already-in-use":
+            setEmailError(error.message); 
+            break; 
+          case "auth/weak-password":
+            setPasswordError(error.message);
+            break;
+          default:
+            authListener(); 
+        }
+    });  
+  };
+
+
   const LogoutHandler = () => { 
-    fire.auth.signOut(); 
+    fire.auth().signOut(); 
   };
 
   const authListener = () => { 
@@ -40,26 +78,47 @@ const Login = () => {
         } else { 
           setCurrentUser(""); 
         }
-
+       
     })
   };
 
-  useEffect(() => { 
+  useEffect(() => {  //not sure if this needs to be here 
     authListener(); 
   });
 
- 
+  const toggleSettings = () => { 
+    setHasAccount(!hasAccount); 
+    clearErrors(); 
+
+  }; 
+  
 
   return (
       <div> 
         {currentUser ? (
           <Home logoutHandler={LogoutHandler} />
-        ) : (
-          <form onSubmit={LoginHandler}>
+        ) : 
+            (
+          <form>
           <div className="login-style">
-            <input type="email" onChange={emailHandler}/>
-            <input type="password" onChange={passwordHandler} value={enteredPassword}/>
-            <button type="submit">Login</button>
+            <label>Username</label>
+            <input type="email" id="username" onChange={emailHandler}/>
+            <label>Password</label>
+            <input type="password" id="pass" onChange={passwordHandler} required value={enteredPassword}/>
+            <p className="errorMsg">{passwordError}{emailError}</p>
+            <div className="btnContainer">
+              {hasAccount ? (
+                <>
+                <button onClick={LoginHandler}>Sign In</button>
+                <p>Don't have an account? <span onClick={toggleSettings}>Sign up</span></p>
+                </> 
+              ) : (
+                <>
+                <button type="submit" onClick={RegistrationHandler}>Register</button>
+                <p>Already have an account? <span onClick={toggleSettings}>Sign In</span></p>
+                </> 
+              )}
+            </div>
           </div>
         </form>
 
